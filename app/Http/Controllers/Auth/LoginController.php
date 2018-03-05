5e8b1e78babf42a601bb4f80c1d84515aab59ef1
hoangnguyen <hoangnguyendctit@gmail.com>
2018-03-05 14:26:15 +0700

    refactor models directory

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\_const\strings;
-use App\User;
+use App\Models\User;
 
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
--- app/Models/motorbike_type.php
+++ app/Models/motorbike_type.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class manufacturer extends Model
+class motorbike_type extends Model
 {
     //
 }
