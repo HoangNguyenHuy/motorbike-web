63239166bcb5d9b864a2042cad3f595555d47fed
hoangnguyen <hoangnguyendctit@gmail.com>
2018-03-06 23:57:29 +0700

    add model user info and add component for form

--- app/Forms/BasicForm.php
+++ app/Forms/BasicForm.php
@@ -16,14 +16,30 @@ class BasicForm
         FormFacade::close();
     }
 
-    function render_text_input($name ,$label='', $default_value='', $placeholder='', $attrs=array()){
+    static function render_text_input($name , $label='', $default_value='', $placeholder='', $attrs=array()){
         $attrs['placeholder']  = $placeholder;
         $html = (
-            FormFacade::label($name, $label).
+            FormFacade::label($name, $label).' : '.
             FormFacade::text($name, $default_value, $attrs)
         );
         return $html;
     }
 
+    static function render_file_input($name, $label='', $attrs=array()){
+        $html = (
+            FormFacade::label($name, $label).' : '.
+            FormFacade::file($name, $attrs)
+        );
+        return $html;
+    }
+
+    static function render_email_field($default_value='', $placeholder='Email Address', $attrs=array()){
+        $attrs['placeholder']  = $placeholder;
+        $html = (
+            FormFacade::label('Email').' : '.
+            FormFacade::email('Email', $default_value, $attrs)
+        );
+        return $html;
+    }
 
 }
---
+++ database/migrations/2018_03_06_160724_create_user_profiles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('user_id');
+            $table->string('name');
+            $table->string('phone_number');
+            $table->boolean('sex');
+            $table->string('address');
+            $table->string('email');
+            $table->string('avatar_url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+}
